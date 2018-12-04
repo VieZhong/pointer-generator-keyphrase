ec797d6dc82ad7dc73aac492c5840830af505c05
VieZhong <zyw13579@qq.com>
2018-12-04 22:02:45 +0800

    fixed bug

--- model.py
+++ model.py
@@ -511,10 +511,17 @@ def _mask_and_avg(values, padding_mask):
     a scalar
   """
 
+  def cal(dec_lens):
+    values_per_step = [v * padding_mask[:, dec_step] for dec_step, v in enumerate(values)]
+    values_per_ex = sum(values_per_step) / dec_lens # shape (batch_size); normalized value for each batch member
+    return tf.reduce_mean(values_per_ex) # overall average
+
   dec_lens = tf.reduce_sum(padding_mask, axis=1) # shape batch_size. float32
-  values_per_step = [v * padding_mask[:,dec_step] for dec_step,v in enumerate(values)]
-  values_per_ex = sum(values_per_step)/dec_lens # shape (batch_size); normalized value for each batch member
-  return tf.reduce_mean(values_per_ex) # overall average
+  tf.cond(
+      tf.less(dec_lens, 1),
+      lambda: 0,
+      lambda: cal(dec_lens)
+  )
 
 
 def _coverage_loss(attn_dists, padding_mask):
