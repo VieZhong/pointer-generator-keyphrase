a7c84742373058b54a2ebc5948609e998b2db5e3
VieZhong <zyw13579@qq.com>
2019-01-06 12:08:26 +0800

    title_engaged

--- attention_decoder.py
+++ attention_decoder.py
@@ -77,12 +77,12 @@ def attention_decoder(decoder_inputs, initial_state, encoder_states, enc_padding
       score_matrix = math_ops.reduce_sum(tf.multiply(tf.tile(tf.expand_dims(encoder_states, -1), [1, 1, 1, title_attn_len]), W_t_c), 3) # batch_size x attn_length x title_attn_size
       score = [] # batch_size x attn_length x title_attn_length
       for batch_index in range(batch_size):
-        score.append(math_ops.reduce_sum(tf.multiply(tf.tile(tf.expand_dims(score_matrix[batch_index], 1), [1, title_attn_len, 1]), title_encoder_states[batch_index])))
+        score.append(math_ops.reduce_sum(tf.multiply(tf.tile(tf.expand_dims(score_matrix[batch_index], 1), [1, title_attn_len, 1]), title_encoder_states[batch_index])), -1)
 
 
       title_attn_dist = nn_ops.softmax(score) # take softmax. shape (batch_size, attn_length, title_attn_length)
       title_attn_dist *= title_padding_mask # apply mask
-      title_masked_sums = tf.reduce_sum(title_attn_dist, axis=2) # shape (batch_size, attn_length)
+      title_masked_sums = tf.reduce_sum(title_attn_dist, axis=2)
       title_attn_dist =  title_attn_dist / tf.reshape(title_masked_sums, [-1, attn_len, 1]) # re-normalize
 
       context_title_states =[] # batch_size x attn_length x title_attn_size
