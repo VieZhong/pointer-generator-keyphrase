4decd220f70456a9ec4897f064f73a62b992c752
VieZhong <zyw13579@qq.com>
2019-01-22 01:50:28 +0000

    max decode num 120

--- decode.py
+++ decode.py
@@ -31,7 +31,7 @@ import hashlib
 FLAGS = tf.app.flags.FLAGS
 
 SECS_UNTIL_NEW_CKPT = 60  # max number of seconds before loading new checkpoint
-DECODE_MAX_NUM = 50
+DECODE_MAX_NUM = 120
 
 
 class BeamSearchDecoder(object):
@@ -345,7 +345,7 @@ def get_decode_dir_name(ckpt_name):
   elif "val" in FLAGS.data_path: dataset = "val"
   elif "test" in FLAGS.data_path: dataset = "test"
   else: raise ValueError("FLAGS.data_path %s should contain one of train, val or test" % (FLAGS.data_path))
-  dirname = "decode_%s_%imaxenc_%ibeam_%imindec_%imaxdec_%imaxnum" % (dataset, FLAGS.max_enc_steps, FLAGS.beam_size, FLAGS.min_dec_steps, FLAGS.max_dec_steps, FLAGS.max_keyphrase_num)
+  dirname = "decode_%s_%imaxenc_%ibeam_%imindec_%imaxdec_%imaxnum" % (dataset, FLAGS.max_enc_steps, FLAGS.beam_size, FLAGS.min_dec_steps, FLAGS.max_dec_steps, DECODE_MAX_NUM)
   if ckpt_name is not None:
     dirname += "_%s" % ckpt_name
   return dirname
