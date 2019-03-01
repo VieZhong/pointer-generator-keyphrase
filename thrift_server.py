5ae04432acd3b8d090444e6b616b349e1984b419
VieZhong <zyw13579@qq.com>
2019-03-01 09:09:52 +0800

    thrift

--- thrift_server.py
+++ thrift_server.py
@@ -189,13 +189,13 @@ class KeyphrasesHandler(object):
     return [ttypes.Keyphrase(r["id"], r["keyphrases"]) for r in decode_results]
 
 
-def main(unused_argv):
-  if len(unused_argv) != 1: # prints a message if you've entered flags incorrectly
-    raise Exception("Problem with flags: %s" % unused_argv)
+# def main(unused_argv):
+#   if len(unused_argv) != 1: # prints a message if you've entered flags incorrectly
+#     raise Exception("Problem with flags: %s" % unused_argv)
   
-  handler = KeyphrasesHandler()
-  x = {"text": "Most existing web video search engines index videos by file names, URLs, and surrounding texts. These types of video metadata roughly describe the whole video in an abstract level without taking the rich content, such as semantic content descriptions and speech within the video, into consideration. Therefore the relevance ranking of the video search results is not satisfactory as the details of video contents are ignored. In this paper we propose a novel relevance ranking approach for Web-based video search using both video metadata and the rich content contained in the videos. To leverage real content into ranking, the videos are segmented into shots, which are smaller and more semantic-meaningful retrievable units, and then more detailed information of video content such as semantic descriptions and speech of each shots are used to improve the retrieval and ranking performance. With video metadata and content information of shots, we developed an integrated ranking approach, which achieves improved ranking performance. We also introduce machine learning into the ranking system, and compare them with IR-model (information retrieval model) based method. The evaluation results demonstrate the effectiveness of the proposed ranking methods.", "id": "relevanceranking", "title": "towards content-based relevance ranking for video search"}
-  handler.predict([x])
+#   handler = KeyphrasesHandler()
+#   x = {"text": "Most existing web video search engines index videos by file names, URLs, and surrounding texts. These types of video metadata roughly describe the whole video in an abstract level without taking the rich content, such as semantic content descriptions and speech within the video, into consideration. Therefore the relevance ranking of the video search results is not satisfactory as the details of video contents are ignored. In this paper we propose a novel relevance ranking approach for Web-based video search using both video metadata and the rich content contained in the videos. To leverage real content into ranking, the videos are segmented into shots, which are smaller and more semantic-meaningful retrievable units, and then more detailed information of video content such as semantic descriptions and speech of each shots are used to improve the retrieval and ranking performance. With video metadata and content information of shots, we developed an integrated ranking approach, which achieves improved ranking performance. We also introduce machine learning into the ranking system, and compare them with IR-model (information retrieval model) based method. The evaluation results demonstrate the effectiveness of the proposed ranking methods.", "id": "relevanceranking", "title": "towards content-based relevance ranking for video search"}
+#   handler.predict([x])
   # processor = KeyphraseModel.Processor(handler)
   # transport = TSocket.TServerSocket(__HOST, __PORT)
   # tfactory = TTransport.TBufferedTransportFactory()
